ebdb8fefc99a7981588c7ef46d94e0d663444b8a
bitt4 <64093718+bitt4@users.noreply.github.com>
2020-11-29 18:10:54 +0100

    Option to add prefix to output filename

--- main.c
+++ main.c
@@ -8,20 +8,26 @@ void display_help();
 int main(int argc, char *argv[]){
 
     char* root_filename = NULL;
+    char* output_filename_format = NULL;
+    char* output_filename_prefix = "wav_";
 
     static struct option long_options[] = {
                                            {"help", no_argument, NULL, 'h'},
+                                           {"output", required_argument, NULL, 'o'},
                                            {NULL, 0, NULL, 0}
     };
 
     int c;
 
-    while ((c = getopt_long(argc, argv, "h", long_options, NULL)) != -1) {
+    while ((c = getopt_long(argc, argv, "ho:", long_options, NULL)) != -1) {
         switch (c)
             {
             case 'h':
                 display_help();
                 exit(EXIT_FAILURE);
+            case 'o':
+                output_filename_prefix = optarg;
+                break;
             case '?':
                 fprintf(stderr,
                         "%s: Use -h or --help to display options.\n", argv[0]);
@@ -62,9 +68,15 @@ int main(int argc, char *argv[]){
     fread(root_file_read_buffer, root_file_size, 1, root_file);
     fclose(root_file);
 
+    unsigned int filename_length = strlen(output_filename_prefix) + 9;    /* "%04u.wav\0" */
+    unsigned int prefix_length = strlen(output_filename_prefix);
+    output_filename_format = (char*)calloc(filename_length, sizeof(char));
+    strncpy(output_filename_format, output_filename_prefix, prefix_length);
+    strncpy(output_filename_format + prefix_length, "%04u.wav", 8);
+
     FILE* current_file = NULL;
-    char filename[16];
     unsigned int file_count = 0;
+    char* filename = (char*)calloc(filename_length, sizeof(char));
 
     const int header_length = 44;
     unsigned int current_file_data_size = 0;
@@ -91,7 +103,7 @@ int main(int argc, char *argv[]){
                 /* Check if data block of current file isn't bigger than length of remaining data in root file */
                 if(root_file_size - pos - header_length >= current_file_data_size){
                     /* fprintf(stdout, "Remaining bytes in data: %lu.\n", root_file_size - pos - header_length); */
-                    snprintf(filename, 15, "wav_%04u.wav", ++file_count);
+                    snprintf(filename, filename_length, output_filename_format, ++file_count);
                     current_file = fopen(filename, "wb");
                     fwrite(root_file_read_buffer + pos, header_length + current_file_data_size, 1, current_file);
                     fclose(current_file);
@@ -108,6 +120,8 @@ int main(int argc, char *argv[]){
         fprintf(stdout, "%u files extracted.\n", file_count);
     }
 
+    free(filename);
+    free(output_filename_format);
     free(root_file_read_buffer);
 
     return 0;
